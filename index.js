9c181a7e1f7cd7031c93471c3c2297e843d73b9e
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-07-15 19:48:59 +0500

    Built RestAPIs and testing with Postman

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require ("express");
+const fs = require('fs');
 const users = require("./MOCK_DATA.json");
 
 
 
 const app = express();
+
+//Middleware - Plugin
+app.use(express.urlencoded({extended:false}));
+
 
 const PORT = 4000;
 
@@ -22,12 +27,18 @@ app.route('/api/users/:id')
     res.json({status:"pending"});
 }).delete((req,res)=>{
    //Delete user with ID 
+    const id = req.params.id;
+    users.pop(id);
     res.json({status:"pending"});
 })
 
-app.post('./api/users', (req,res)=>{
-    //TODO: Create new user
-    return res.send({status:"pending"})
+app.post('/api/users', (req,res)=>{
+    const body = req.body;  //The request/content sent from the frontend side comes in req.body
+    users.push({...body,id:users.length + 1});
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err,dats)=>{
+      return res.send({status:"pending",id:users.length});
+    })
+    
 })
 
-app.listen(PORT, ()=>console.log("Server Started!"))
+app.listen(PORT, ()=>console.log("Server Started at port:"+PORT));
